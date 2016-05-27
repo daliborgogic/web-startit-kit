3348cdcfe909ce8a5dad0cee29d586e1badad0c6
Dalibor <mail@daliborgogic.com>
2016-05-27 14:02:16 +0200

    browser-sync support

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
+var browserSync = require('browser-sync').create()
 var sourcemaps = require('gulp-sourcemaps')
+
+gulp.task('serve', ['sass'], function () {
+  browserSync.init({
+    server: ['app', 'dist']
+  })
+  gulp.watch('app/sass/*.scss', ['sass'])
+  gulp.watch('app/*.html').on('change', browserSync.reload)
+})
 
 gulp.task('sass', function () {
   return gulp.src('./app/sass/**/*.scss')
@@ -8,12 +17,7 @@ gulp.task('sass', function () {
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./dist/css'))
+    .pipe(browserSync.stream())
 })
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./app/sass/**/*.scss', ['sass'])
-})
-
-gulp.task('default', ['sass:watch'], function () {
-  console.log('Foo')
-})
+gulp.task('default', ['serve'])
