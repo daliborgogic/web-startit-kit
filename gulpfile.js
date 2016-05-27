85f9723572beabf0b227671cb2b7bdcdbf1232c0
Dalibor <mail@daliborgogic.com>
2016-05-27 14:33:48 +0200

    JavaScript Standard Linter support

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,14 @@ var sass = require('gulp-sass')
 var browserSync = require('browser-sync').create()
 var sourcemaps = require('gulp-sourcemaps')
 var autoprefixer = require('gulp-autoprefixer')
+var standard = require('gulp-standard')
 
-gulp.task('serve', ['sass'], function () {
+gulp.task('serve', ['sass', 'standard'], function () {
   browserSync.init({
     server: ['.tmp', 'app']
   })
   gulp.watch('app/sass/*.scss', ['sass'])
+  gulp.watch('app/js/*.js', ['standard'])
   gulp.watch('app/*.html').on('change', browserSync.reload)
 })
 
@@ -20,6 +22,16 @@ gulp.task('sass', function () {
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./tmp/css'))
     .pipe(browserSync.stream())
+})
+
+// Checking JavaScript code with the standard syntax
+// https://www.npmjs.com/package/gulp-standard
+gulp.task('standard', function () {
+  return gulp.src(['./app/js/main.js'])
+    .pipe(standard())
+    .pipe(standard.reporter('default', {
+      breakOnError: true
+    }))
 })
 
 gulp.task('default', ['serve'])
